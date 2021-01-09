4fb7a25f541dec8a8772ef161f786cd4193759f0
Chris Risseler <chrisrisseler43@gmail.com>
2021-01-09 12:33:32 -0500

    added build queryurl

--- assets/js/nytCode.js
+++ assets/js/nytCode.js
@@ -2,15 +2,49 @@
  * pulls information from the form and build the query URL
  * @returns {string} URL for NYT API based on form inputs
  */
+
+var query = $("#search-term").val()
+var beginDate = $("#start-year").val()
+var endDate = $("#end-year").val()
+
+
+// var beginDate = "2020"
+// query = "elections"
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&begin_date=" + beginDate + "&api-key=S5QfG34XsbEFdDAFYXy4SCV4EvNGfjrm"
+
+
 function buildQueryURL() {
+
+  if (beginDate === false && endDate === false) { queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=S5QfG34XsbEFdDAFYXy4SCV4EvNGfjrm" }
+
+  else if (beginDate === true && endDate === false) {
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&begin_date=" + beginDate + "0101&api-key=S5QfG34XsbEFdDAFYXy4SCV4EvNGfjrm"
+  }
+
+  else if (beginDate === false && endDate === true)
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&end_date=" + endDate + "1231&api-key=S5QfG34XsbEFdDAFYXy4SCV4EvNGfjrm"
+
+  else {
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&begin_date=" + beginDate + "0101&end_date=" + endDate + "1231&api-key=S5QfG34XsbEFdDAFYXy4SCV4EvNGfjrm"
+  }
+
+  return queryURL;
 
 }
 
+// console.log(queryURL)
 /**
  * takes API data (JSON/object) and turns it into elements on the page
  * @param {object} NYTData - object containing NYT API data
  */
+
+
+
+
 function updatePage(NYTData) {
+
+
+
 
 }
 
